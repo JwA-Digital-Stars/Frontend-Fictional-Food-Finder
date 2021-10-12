4c49386173fb46350c69bf4df76f0136cbba8dff
Jose Gomez <jose.gomez@revature.net>
2021-10-11 19:59:07 -0600

    Commiting new components

--- Angular-Fictional-Food-Finder/src/app/app-routing.module.ts
+++ Angular-Fictional-Food-Finder/src/app/app-routing.module.ts
@@ -3,16 +3,32 @@ import { RouterModule, Routes } from '@angular/router';
 import{DashboardComponent} from 'src/app/components/dashboard/dashboard.component'
 import { TruckFinderComponent } from './components/truck-finder/truck-finder.component';
 import { TruckFavoritesComponent } from './components/truck-favorites/truck-favorites.component';
+import { LogInComponent } from './components/log-in/log-in.component';
+import { DashboardOwnerComponent } from './components/dashboard-owner/dashboard-owner.component';
+import { RegisterCustomerComponent } from './components/register-customer/register-customer.component';
+import { RegisterOwnerComponent } from './components/register-owner/register-owner.component';
+
+
 const routes: Routes = [
 
   //this is the default path for the application. This can be changed to another component.
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  //this is the path for dashboard
+  { path: '', redirectTo: '/Login', pathMatch: 'full' },
+  //this is the path for dashboard for the customer, this is more like a home.
   { path: 'dashboard', component: DashboardComponent },
   //path for TruckFinder component. Implemented with a routerLink @ dashboard.html
   {path: 'LocatingFoodTruck', component: TruckFinderComponent},
   //path for favorites component. Implemented with a routerLink @ dashboard.html
-  {path: 'Favorites', component: TruckFavoritesComponent}
+  {path: 'Favorites', component: TruckFavoritesComponent},
+  // log in component for both customers and owners.
+  {path: 'Login', component: LogInComponent},
+  //Actual dashboard for owner
+  {path: 'ownerDashboard', component: DashboardOwnerComponent},
+  //register for customer
+  {path: 'registerCustomer', component:RegisterCustomerComponent},
+  //register for owner
+  {path: 'registerOwner', component:RegisterOwnerComponent}
+
+
 ];
 
 @NgModule({
--- Angular-Fictional-Food-Finder/src/app/app.module.ts
+++ Angular-Fictional-Food-Finder/src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { TruckFinderComponent } from './components/truck-finder/truck-finder.component';
 import { TruckFavoritesComponent } from './components/truck-favorites/truck-favorites.component';
+import { LogInComponent } from './components/log-in/log-in.component';
+import { DashboardOwnerComponent } from './components/dashboard-owner/dashboard-owner.component';
+import { RegisterCustomerComponent } from './components/register-customer/register-customer.component';
+import { RegisterOwnerComponent } from './components/register-owner/register-owner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
     TruckFinderComponent,
-    TruckFavoritesComponent
+    TruckFavoritesComponent,
+    LogInComponent,
+    DashboardOwnerComponent,
+    RegisterCustomerComponent,
+    RegisterOwnerComponent
   ],
   imports: [
     BrowserModule,
