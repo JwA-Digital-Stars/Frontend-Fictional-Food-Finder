df1c259aa9224a717b88b6f474183157e8dfe22c
Jose Gomez <jose.gomez@revature.net>
2021-10-13 18:33:02 -0600

    commit new html

--- Angular-Fictional-Food-Finder/src/app/components/truck-favorites/truck-favorites.component.ts
+++ Angular-Fictional-Food-Finder/src/app/components/truck-favorites/truck-favorites.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-truck-favorites',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TruckFavoritesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _location: Location) { }
 
   ngOnInit(): void {
+  }
+
+  goBack(){
+    this._location.back();
   }
 
 }
--- Angular-Fictional-Food-Finder/src/app/components/truck-finder/truck-finder.component.ts
+++ Angular-Fictional-Food-Finder/src/app/components/truck-finder/truck-finder.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-truck-finder',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TruckFinderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _location: Location) { }
 
   ngOnInit(): void {
   }
 
+  goBack(){
+    this._location.back();
+  }
 }
