37700a0e97cf7ed3880db6e8ad1bf4332ab765d1
Jose Gomez <jose.gomez@revature.net>
2021-10-16 13:44:58 -0600

    new commit

--- Angular-Fictional-Food-Finder/src/app/models/Truck.ts
+++ Angular-Fictional-Food-Finder/src/app/models/Truck.ts
@@ -4,7 +4,9 @@ import { TruckOwner } from "./TruckOwner";
 export class Truck{
 
     constructor(public name:string, public hours:string, public owner:TruckOwner){
-
+            this.name = name;
+            this.hours = hours;
+            this.owner = owner;
     }
 
 
